20754946d438feaf0ef5f0b56b8277947696071c
Jade Neff <njade79162@gmail.com>
2022-01-26 21:23:24 -0500

    Add project files.

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,112 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerID = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUTC = DateTimeOffset.Now
+                };
+
+            using(var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerID == _userId)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteId = e.NoteID,
+                                    Title = e.Title,
+                                    CreatedUTC = e.CreatedUTC
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteID == id && e.OwnerID == _userId);
+                return
+                    new NoteDetail
+                    {
+                        NoteID = entity.NoteID,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUTC = entity.CreatedUTC,
+                        ModifiedUTC = entity.ModifiedUTC
+                    };
+            }
+        }
+
+        public bool UpdateNote(NoteEdit model)
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteID == model.NoteId && e.OwnerID == _userId);
+
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUTC = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteNote(int noteId)
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteID == noteId && e.OwnerID == _userId);
+
+                ctx.Notes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
